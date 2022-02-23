2cdd3cc6200196ea54b4c97800a115d9f902df29
Andrey <91373803+S-404@users.noreply.github.com>
2022-02-23 14:11:56 +0300

    add personal data

---
+++ src/store/EN/data.js
@@ -0,0 +1,119 @@
+export const dataEN = {
+    personalData: {
+        name: 'Andrey Sudarev',
+        title: 'Junior Frontend Developer',
+        image: '',
+        contacts: [
+            {type: 'email', value: 'sudarev.andrey.i@gmail.com'},
+            {type: 'phone', value: '+7 (967) 123 50 80'},
+            {type: 'location', value: 'Moscow'},
+            {type: 'skype', value: '+7 (967) 123 50 80'},
+            {type: 'github', value: 'https://github.com/S-404'},
+        ]
+    },
+    skills: {
+        title: 'Skills',
+        content: [
+            {
+                type: 'JavaScript(ES6+)',
+                points: ['React', 'Redux', 'TypeScript', 'Node', 'JQuery']
+            },
+            {
+                type: 'HTML',
+                points: ['BEM']
+            },
+            {
+                type: 'CSS',
+                points: ['SASS']
+            },
+            {
+                type: 'SQL',
+                points: ['MSSQL', 'MySQL']
+            },
+            {
+                type: 'Tools',
+                points: ['Git/GitHub', 'npm']
+            },
+            {
+                type: 'Others',
+                points: ['PowerShell', 'PHP', 'vb.NET']
+            },
+        ]
+
+    },
+    about: {
+        title: 'About me',
+        content: [
+            {
+                type: 'web development',
+                value: 'Joined web application development in 2020'
+            },
+            {
+                type: 'frontend',
+                value: 'JQuery (since 2020), React (since Nov 2021)'
+            },
+            {
+                type: 'backend',
+                value: 'PHP, Node.js, server machine automation with PowerShell scripts , ' +
+                    'SQL server administration (MSSQL)'
+            },
+            {
+                type: 'other',
+                value: 'Developing programming skills since 2017\n' +
+                    'started with vb.NET (win forms)\n' +
+                    'and small С# UNITY projects'
+            },
+        ]
+    },
+    sections: [
+        {
+            title: 'Experience',
+            content: [
+                {
+                    title: 'Logistics, supply support',
+                    period: 'November 2011 - currently',
+                    organization: 'IKEA',
+                    description: [
+                        'Fullstack developing of web portal, ' +
+                        'Development of web applications to optimize and automate the processes of a commercial team',
+                        'Database design',
+                        'Configure and maintain of web and SQL servers and environments',
+                        'Using internal systems to analyze and maintain store stock availability',
+                        'Make reports for the analysis of key indicators with a view to their subsequent improvement',
+                        'Organizing and conducting an inventory of goods, investigating discovered discrepancies',
+                        'Maintaining internal document flow',
+                        'Handling internal transactions',
+                    ],
+                    achievements: {
+                        title: 'Achievements',
+                        points: [
+                            'I apply my programming skills to achieve business goals ' +
+                            'by developing methods and tools for the interaction of a commercial team',
+                            'Reached the role Senior Specialist',
+                            'Optimization of department processes - ' +
+                            'created and implemented applications ' +
+                            'that reduced the time spent on routines',
+                        ]
+                    }
+                }
+            ]
+        },
+        {
+            title: 'Education',
+            content: [
+                {
+                    title: 'State and municipal administration, Strategic management',
+                    period: 'Year of graduation: 2012',
+                    organization: 'НОУ ВПО Профессиональный Институт Юриспруденции',
+                    description: [],
+                    achievements: {
+                        title: 'Achievements',
+                        points:[]
+                    },
+                }
+            ]
+
+        }
+    ]
+}
+
