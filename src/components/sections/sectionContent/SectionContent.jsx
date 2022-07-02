478d4b948a494d461d37fa78ef977d3b81b9829b
Andrey <sudarev.andrey.i@gmail.com>
2022-07-02 13:58:58 +0300

    upd SectionContent - add 'website'

--- src/components/sections/sectionContent/SectionContent.jsx
+++ src/components/sections/sectionContent/SectionContent.jsx
@@ -6,6 +6,16 @@ const SectionContent = ({content}) => {
     return (
         <article className='section-contents__content'>
             <h3 className='content__organization'>{content.organization}</h3>
+            {content.webSite ?
+                <a
+                    href={content.webSite}
+                    target='_blank'
+                    rel="noreferrer"
+                    className='content__organization-website'
+                >
+                    {content.webSite}
+                </a>
+                : null}
             <span className='content__period'>{content.period}</span>
             <h3 className='content__title'>{content.title}</h3>
             <Description content={content}/>
